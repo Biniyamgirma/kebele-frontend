38eb204cb5d27e89e560adfedb0379b1218408ec
Biniyamgirma <biniyam0928@gmail.com>
2026-03-14 04:44:55 +0300

    i think i found it the bug is found on 4:44AM i will sleep peace fully

--- src/components/admin/AdministratorCard.tsx
+++ src/components/admin/AdministratorCard.tsx
@@ -1,18 +1,21 @@
 // components/admin/AdministratorCard.tsx
 import React from "react";
-import { Card, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 
 interface Props {
   item: any;
-  key:number;
-  handleCardClick:()=>any;
+  key: number;
+  handleCardClick: () => any;
 }
 
-const AdministratorCard: React.FC<Props> = ({ item,key,handleCardClick }) => {
-  return (
-    
-  );
+const AdministratorCard: React.FC<Props> = ({ item, key, handleCardClick }) => {
+  return <div></div>;
 };
 
 export default AdministratorCard;
